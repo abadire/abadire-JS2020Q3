a92b4e4cb8576aa9d65149e9afa37e6ab544f745
abadire <panvan97@gmail.com>
2020-09-24 23:01:21 +0300

    feat: add decimal separators

--- calculator/script.js
+++ calculator/script.js
@@ -2,7 +2,16 @@ let previous = document.getElementsByClassName("display__prev")[0];
 let current = document.getElementsByClassName("display__cur")[0];
 let num_btns = [];
 let previous_str = "";
-let current_str = "";
+let current_str = "0";
+current.textContent = "0";
+
+function setSeparators(str) {
+  let arr = [];
+  for (let i = 0; i + 3 < str.length; i += 3) {
+    arr.unshift(str.slice(str.length - i - 4, str.length - i - 1));
+  }
+  return arr.join();
+}
 
 for (let i = 0; i < 10; ++i)
 {
@@ -16,14 +25,23 @@ for (let i = 0; i < 10; ++i)
     else if (current_str.length < 9) {
       current_str += i;
     }
-    current.innerText = current_str;
+    current.innerText = Number(current_str).toLocaleString();
   });
 }
 
-document.getElementsByClassName("numpad__btn--del")[0].addEventListener("click", function() {
-  current_str = "";
+document.getElementsByClassName("numpad__btn--ac")[0].addEventListener("click", function() {
+  current_str = "0";
   previous_str = "";
-  current.style.fontSize = "1em";
-  current.textContent = "";
+  current.style.fontSize = "1.1em";
+  current.textContent = "0";
   previous.textContent = "";
+})
+
+document.getElementsByClassName("numpad__btn--del")[0].addEventListener("click", function () {
+  if (current_str != "0") {
+    if (current_str.length > 1)
+      current_str = current_str.slice(0, current_str.length-1);
+    else current_str = "0"; 
+    current.textContent = Number(current_str).toLocaleString();
+  }
 })
