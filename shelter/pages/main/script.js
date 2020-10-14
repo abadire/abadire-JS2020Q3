70fb91f29738f66e288e697576899e4b0d36f200
abadire <panvan97@gmail.com>
2020-10-14 12:58:00 +0300

    feat: finish main response

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -13,3 +13,16 @@ for (let i = 0; i < links.length; ++i)
     });
   }
 }
+
+document.getElementsByClassName('navigation__button')[0].addEventListener('click', function () {
+  document.getElementsByClassName('navigation__overlay')[0].style.width = '100vw';
+  document.getElementsByClassName('navigation__overlay')[0].style.opacity = '1';
+});
+
+document.getElementsByClassName('navigation__overlay')[0].addEventListener('click', function () {
+  if (document.getElementById('toggle').checked) {
+    document.getElementById('toggle').checked = false;
+    this.style.opacity = '0';
+    setTimeout(() => this.style.width = '0', 500);
+  }
+});
