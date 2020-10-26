74bff1a9fc87a9a5123f9bf7466687ed82f8dd67
abadire <panvan97@gmail.com>
2020-10-26 13:40:06 +0300

    feat: add logo on menu slider

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,4 +1,5 @@
 const links = document.getElementsByClassName("navigation__link");
+const logo = document.getElementsByClassName("logo")[0];
 const linkAbout = document.getElementsByClassName("navigation__link--active")[0];
 const overlay = document.getElementsByClassName("navigation__overlay")[0];
 const navigationBurger = document.getElementsByClassName("navigation__button")[0];
@@ -22,22 +23,48 @@ navigationBurger.addEventListener('click', function () {
   {
     isMenuShown = false;
     navigationBurger.style.transform = '';
+    navigationBurger.style.top = '';
     document.body.style.overflow = 'visible';
+    
+    // Move logo back
+    logo.style.opacity = '0';
+    setTimeout(() => {
+      logo.style.left = '';
+      logo.style.top = '0';
+    }, 300);
+    setTimeout(() => logo.style.opacity = '1', 300);
+    
+    // Hide overlay
     overlay.style.opacity = '0';
     setTimeout(() => overlay.style.width = '0', 500);
   }
   else
   {
     isMenuShown = true;
+    
+    // Show overlay
     overlay.style.width = '100vw';
     overlay.style.opacity = '1';
+    
     navigationBurger.style.transform = 'rotate(90deg)';
+    navigationBurger.style.top = `${window.scrollY}px`;
+    
+    // Move logo
+    logo.style.opacity = '0';
+    setTimeout(() => {
+      logo.style.left = `${window.innerWidth - 320}px`;
+      logo.style.top = `${window.scrollY}px`;
+    }, 300);
+    setTimeout(() => logo.style.opacity = '1', 300);
+    
+    // Prevent scroll
     document.body.style.overflow = 'hidden';
   }
 });
 
 overlay.addEventListener('click', function() {
   if (document.getElementById('toggle').checked) {
+    isMenuShown = false;
     document.getElementById('toggle').checked = false;
     navigationBurger.style.transform = '';
     document.body.style.overflow = 'visible';
--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,4 +1,5 @@
 const links = document.getElementsByClassName("navigation__link");
+const logo = document.getElementsByClassName("logo")[0];
 const linkPets = document.getElementsByClassName("navigation__link--active")[0];
 const overlay = document.getElementsByClassName("navigation__overlay")[0];
 const navigationBurger = document.getElementsByClassName("navigation__button")[0];
@@ -23,21 +24,42 @@ navigationBurger.addEventListener('click', function () {
     isMenuShown = false;
     navigationBurger.style.transform = '';
     document.body.style.overflow = 'visible';
+    
+    // Move logo
+    logo.style.opacity = '0';
+    setTimeout(() => {
+      logo.style.left = '';
+    }, 300);
+    setTimeout(() => logo.style.opacity = '1', 300);
+    
     overlay.style.opacity = '0';
     setTimeout(() => overlay.style.width = '0', 500);
   }
   else
   {
     isMenuShown = true;
+    
+    // Show overlay
     overlay.style.width = '100vw';
     overlay.style.opacity = '1';
+    
     navigationBurger.style.transform = 'rotate(90deg)';
+    
+    // Move logo
+    logo.style.opacity = '0';
+    setTimeout(() => {
+      logo.style.left = `${window.innerWidth - 320}px`;
+    }, 300);
+    setTimeout(() => logo.style.opacity = '1', 300);
+    
+    // Prevent scroll
     document.body.style.overflow = 'hidden';
   }
 });
 
 overlay.addEventListener('click', function() {
   if (document.getElementById('toggle').checked) {
+    isMenuShown = false;
     document.getElementById('toggle').checked = false;
     navigationBurger.style.transform = '';
     document.body.style.overflow = 'visible';
